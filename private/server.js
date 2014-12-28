248612d2f83f1e1f7ec7cc63d9ed86ef99818ffc
Sohail Alam <sohailalam2@hotmail.com>
2014-12-28 06:36:40 +0000

    enryption added for db username/password

--- private/server.js
+++ private/server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var jobModel = require("./models/Job");
 var jobsData = require("./jobs-data");
+var aes = require('aes-helper');
 
 var app = express();
 
@@ -15,8 +16,16 @@ app.get("*", function(req, res) {
     res.render("index");
 });
 
-// jobsData.connectDB('mongodb://localhost/jobfinder');
-jobsData.connectDB('mongodb://dev:dev@dbh86.mongolab.com:27867/jobfinder')
+var PRIVATE_PASSWORD = process.env.PRIVATE_PASSWORD;
+if (!PRIVATE_PASSWORD) {
+    console.error('Please set the environment variable PRIVATE_PASSWORD');
+    process.exit(-1);
+}
+
+var decodedUsername = aes.decrypt("6b8e11384ff1a6ee8d370dd6644d7db5", PRIVATE_PASSWORD);
+var decodedPassword = aes.decrypt("2b904803a06895640f7726a71bc1e234", PRIVATE_PASSWORD);
+
+jobsData.connectDB('mongodb://' + decodedUsername + ':' + decodedPassword + '@dbh86.mongolab.com:27867/jobfinder')
     .then(function() {
         console.log('connected to mongodb successfully');
         jobsData.seedJobs();
